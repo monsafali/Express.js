5d4206c75688a6ff5ed011e83348d6b9e45495a5
“monsafali” <monsafali272@gmail.com>
2025-04-21 09:00:00 +0500

    updated funtionality added

--- 02-crud_opration/index.js
+++ 02-crud_opration/index.js
@@ -7,10 +7,12 @@ import { v4 as uuidv4 } from "uuid";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import bodyParser from "body-parser";
+import methodOverride from "method-override";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 let posts = [
   { id: uuidv4(), name: "monsaf", Description: "i love Mern STackprograming" },
@@ -40,6 +42,19 @@ app.get("/post/:id", (req, res) => {
   res.render("show.ejs", { post });
 });
 
+app.patch("/post/:id", (req, res) => {
+  let newDescription = req.body.Description;
+  let { id } = req.params;
+  let post = posts.find((onepost) => onepost.id === id);
+  post.Description = newDescription;
+  res.redirect("/post");
+});
+
+app.get("/post/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((post) => post.id === id);
+  res.render("Update.ejs", { post });
+});
 app.listen(port, () => {
   console.log("Server successfuly started");
 });
