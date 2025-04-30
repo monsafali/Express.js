60ccfebfd706312bfde3d0e890fc4d9fe2c4b269
monsafali <monsafali272@gmail.com>
2025-04-30 16:41:54 +0500

    wrap async aded

--- 03_mongose/init.js
+++ 03_mongose/init.js
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
-import path from "path";
 import Chat from "./models/Chat.js";
 import RandomChats from "./RandomChat.js";
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
 
 main()
   .then((res) => {
     console.log("Connection successfuly Established");
   })
   .catch((err) => console.log(err));
-
-async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
-}
 
 Chat.insertMany(RandomChats)
   .then((res) => console.log("Data saved successfuly"))
