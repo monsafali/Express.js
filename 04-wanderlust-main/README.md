f0b66424cbde9433757302f306b27f63fd79ba0b
“monsafali” <monsafali272@gmail.com>
2025-05-08 08:07:29 +0500

    passwort-mongoose

---
+++ 04-wanderlust-main/README.md
@@ -0,0 +1,3 @@
+npm i passport-local-mongoose
+npm i passport-local
+npm i passport
--- 04-wanderlust-main/app.js
+++ 04-wanderlust-main/app.js
@@ -14,6 +14,9 @@ const listing = require("./routes/listings.js");
 const review = require("./routes/reviews.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const sessionOptions = {
   secret: "mysupersecretstring",
@@ -46,6 +49,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
