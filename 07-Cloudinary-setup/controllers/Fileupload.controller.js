aa755c87169bc265253cac368ff2d18e8934faf8
“monsafali” <monsafali272@gmail.com>
2025-05-28 11:35:16 +0500

    how to uploade  image into cloudinary & dbs as well

--- 07-Cloudinary-setup/controllers/Fileupload.controller.js
+++ 07-Cloudinary-setup/controllers/Fileupload.controller.js
@@ -1,30 +1,7 @@
-// const File = require("../models/File.Model");
-
-// const localFileUpload = async (req, res) => {
-//   try {
-//     const file = req.files.file;
-//     console.log("file agye hai", file);
-
-//     let path = __dirname + "/file" + Date.now() + `.${file.name.split(".")[1]}`;
-//     console.log("PAth is", path);
-
-//     file.mv(path, (err) => {
-//       console.log(err);
-//     });
-
-//     res.json({
-//       success: true,
-//       message: "Local File UPload Successfuly ",
-//     });
-//   } catch (error) {
-//     console.log("something went wrong", error);
-//   }
-// };
-
-// module.exports = localFileUpload;
-
 const path = require("path");
+const File = require("../models/File.Model");
 const fs = require("fs");
+const cloudinary = require("cloudinary").v2;
 
 const localFileUpload = async (req, res) => {
   try {
@@ -71,4 +48,59 @@ const localFileUpload = async (req, res) => {
   }
 };
 
-module.exports = localFileUpload;
+function isFileTypeSupported(filetype, supportedTypes) {
+  return supportedTypes.includes(filetype);
+}
+
+async function uploadFileToCloudinary(file, folder) {
+  const options = { folder };
+  return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+// Image upload Handlewr=
+const imageuploader = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    // console.log(name, email, tags);
+
+    const file = req.files.imageFile;
+    // console.log(file);
+
+    //  Validation krni hai
+    const supportedTypes = ["jpg", "jpeg", "png"];
+
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    if (!isFileTypeSupported(fileType, supportedTypes))
+      return res.status(400).json({
+        success: false,
+        message: "File type not supported",
+      });
+
+    // File formate
+
+    const response = await uploadFileToCloudinary(file, "codehelp");
+    console.log(response);
+    // Db men entry save krni haai
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.status(200).json({
+      success: true,
+      imageUrl: response.secure_url,
+      message: "image succesfuly uploaed ",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      success: false,
+      message: "Someting went wrong ",
+    });
+  }
+};
+
+module.exports = { imageuploader, localFileUpload };
--- 07-Cloudinary-setup/routes/Fileupload.js
+++ 07-Cloudinary-setup/routes/Fileupload.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const localFileUpload = require("../controllers/Fileupload.controller");
+const {
+  localFileUpload,
+  imageuploader,
+} = require("../controllers/Fileupload.controller");
 
 router.post("/localFileUpload", localFileUpload);
+router.post("/imageuploader", imageuploader);
 
 module.exports = router;
