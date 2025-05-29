f4128eda6b9d9ffda9ef1bcee0fe2e4cf5be13d5
“monsafali” <monsafali272@gmail.com>
2025-05-29 10:19:54 +0500

    nodemailer aadded

--- 07-Cloudinary-setup/models/File.Model.js
+++ 07-Cloudinary-setup/models/File.Model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const FileSchema = new mongoose.Schema({
   name: {
@@ -15,6 +16,32 @@ const FileSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+});
+
+// post middleware
+FileSchema.post("save", async function (doc) {
+  try {
+    console.log("Doc", doc);
+
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    // Send emaijl
+    let info = await transporter.sendMail({
+      from: "Codehlpe",
+      to: doc.email,
+      subject: "New file upload on Cloudinary",
+      html: `<h2>Hello ${doc.name} Your file is has been uploaded successfuly. <p> <a href="${doc.imageUrl}">See</a> </p></h2>`,
+    });
+    console.log(info);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 const File = mongoose.model("File", FileSchema);
